aefec31e70fd56d727e4c3923640ac59a5ff8dc6
anahrendra <anahrendra@kaist.ac.kr>
2025-08-20 12:17:34 +0900

    update conda

--- zsh/conda.sh
+++ zsh/conda.sh
@@ -4,10 +4,10 @@ __conda_setup="$('/home/aswin2/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "~/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "~/anaconda3/etc/profile.d/conda.sh"
+    if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/anaconda3/etc/profile.d/conda.sh"
     else
-        export PATH="~/anaconda3/bin:$PATH"
+        export PATH="$HOME/anaconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
